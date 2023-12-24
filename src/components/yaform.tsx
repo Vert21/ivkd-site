8af50c3fc471a18a3f591f06c139aca6c2ced7b7
knowset <knowset@proton.me>
2023-12-24 23:22:24 +0300

    added some fixes and a few bugs

--- src/components/yaform.tsx
+++ src/components/yaform.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 
 export const YaForm: FC<{}> = () => {
   return (
-    <div className="w-full sm:w-[80%] md:w-[70%] lg:w-[60%]">
+    <div className="w-full sm:w-[70%] md:w-[60%] lg:w-[50%] xl:w-[40%] 2xl:w-[30%]">
       <iframe
         src="https://forms.yandex.ru/u/6562318ee010db5695ea17e8/?iframe=1"
         name={"ya-form-6562318ee010db5695ea17e8"}
