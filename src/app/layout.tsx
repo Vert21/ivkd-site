eac53ad5be06722bf798a2a6509221d17d9b34bc
knowset <knowset@proton.me>
2024-01-14 16:13:48 +0300

    added some fixes and a few bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,27 @@
 import "./globals.css";
+import Favicon_16 from "../../public/favicon-16x16.png";
+import Favicon_32 from "../../public/favicon-32x32.png";
+import Favicon_Apple_Touch from "../../public/apple-touch-icon.png";
 
 export const metadata = {
   metadataBase: new URL("https://engineer-v-dom.vercel.app"),
   title: "ИНЖЕНЕР В КАЖДЫЙ ДОМ",
   description: "",
+  icons: [{
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    url: Favicon_16.src,
+  }, {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    url: Favicon_32.src,
+  }, {
+    rel: "apple-touch-icon",
+    sizes: "180x180",
+    url: Favicon_Apple_Touch.src,
+  }],
   openGraph: {
     url: "https://engineer-v-dom.vercel.app",
     title: "ИНЖЕНЕР В КАЖДЫЙ ДОМ",
